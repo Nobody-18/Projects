9b9419a4396dbcc355cab5da977a537668186669
Nobody <ajulanilkumar2442>
2022-10-24 14:31:43 +0530

    pdf merger

--- PDF_merger/pdfmerge.py
+++ PDF_merger/pdfmerge.py
@@ -5,5 +5,20 @@ merger = pdf.PdfFileMerger()
 
 for file in os.listdir(os.curdir):
     if file.endswith(".pdf"):
-        merger.append(file)
-merger.write("combinedBSUniDocs.pdf")
+        merger.append(file) 
+
+
+merger.write("combined.pdf")
+# reader = pdf.PdfReader("combined.pdf")
+# writer = pdf.PdfWriter()
+# for page in reader.pages:
+#     page.compress_content_streams()
+#     writer.add_page(page)
+# page = reader.pages[0]
+# print(page.extract_text())
+# writer.add_metadata(reader.metadata)
+# writer.remove_images()
+
+
+# with open("compressed.pdf", "wb") as pf:
+#     writer.write(pf)
